be185c818de1698e2fb0a61b1c5d794bebf47e22
Kholov Rustam <rustamkholow5@gmail.com>
2025-06-10 19:11:35 +0300

    check point

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import FeatureOverview from './components/FeatureOverview/FeatureOverview'
 import FinancialNewsBlock from './components/FinancialNewsBlock/FinancialNewsBlock'
 import GlobalAvailability from './components/GlobalAvailability/GlobalAvailability'
 import Header from './components/Header/Header'
+import SupportNewsletterSignup from './components/SupportNewsletterSignup/SupportNewsletterSignup'
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <CurrencyRate />
       <GlobalAvailability />
       <FinancialNewsBlock />
+      <SupportNewsletterSignup />
     </>
   )
 }
---
+++ src/components/SupportNewsletterSignup/SupportNewsletterSignup.tsx
@@ -0,0 +1,23 @@
+import EnvelopeIcon from '../../assets/icons/EnvelopeIcon'
+import PlaneIcon from '../../assets/icons/PlaneIcon'
+import styles from './support-newsletter-signup.module.scss'
+
+const SupportNewsletterSignup = () => {
+  return (
+    <section className={styles.supporSection}>
+      <h6 className={styles.supportTitle}>Support</h6>
+      <h4 className={styles.newsletterTitle}>Subscribe Newsletter & get</h4>
+      <h5 className={styles.bankNewsTitle}>Bank News</h5>
+      <div className={styles.inputWrapper}>
+        <EnvelopeIcon className={styles.envelopeIcon} />
+        <input type="text" className={styles.input} />
+        <div className={styles.subscribeWrapper}>
+          <PlaneIcon className={styles.planeIcon} />
+          <button className={styles.subscribeButton}>Subscribe</button>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default SupportNewsletterSignup
