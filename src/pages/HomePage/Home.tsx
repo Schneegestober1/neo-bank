3e328549aa2b7e913484971d481ed8c4f81d76cc
Kholov Rustam <rustamkholow5@gmail.com>
2025-06-17 01:07:11 +0300

    CurrencyRate

--- src/pages/HomePage/Home.tsx
+++ src/pages/HomePage/Home.tsx
@@ -1,10 +1,10 @@
 
 import FeatureOverview from '../../components/FeatureOverview/FeatureOverview'
-import CurrencyRate from '../../components/CurrencyRate/CurrencyRate'
 import GlobalAvailability from '../../components/GlobalAvailability/GlobalAvailability'
 import FinancialNewsBlock from '../../components/FinancialNewsBlock/FinancialNewsBlock'
 import SupportNewsletterSignup from '../../components/SupportNewsletterSignup/SupportNewsletterSignup'
 import CardPromoTitle from '../../components/home/CardPromoTitle/CardPromoTitle'
+import CurrencyRate from '../../components/home/CurrencyRate/CurrencyRate'
 
 const Home = () => {
   return (
