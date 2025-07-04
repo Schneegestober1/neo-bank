95c122c40c7c6746d1515bf523f613ccc0f8d6a3
Kholov Rustam <rustamkholow5@gmail.com>
2025-07-04 13:26:03 +0300

    credit-card-benefits__nav is almost ready

--- src/components/loan/CreditCardBenefits/CreditCardBenefits.tsx
+++ src/components/loan/CreditCardBenefits/CreditCardBenefits.tsx
@@ -1,31 +1,75 @@
+import MoneyDuotoneIcon from '../../../assets/icons/MoneyDuotoneIcon'
 import styles from '../CreditCardBenefits/credit-card-benefits.module.scss'
 
 const CreditCardBenefits = () => {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <a href="">About card</a>
+    <div className={styles['credit-card-benefits']}>
+
+      <nav className={styles['credit-card-benefits__nav']}>
+        <ul className={styles['credit-card-benefits__nav-list']}>
+          <li className={styles['credit-card-benefits__nav-item']}>
+            <a href="" className={styles['credit-card-benefits__nav-link']}>
+              About card
+            </a>
           </li>
-          <li>
-            <a href="">Rates and conditions</a>
+          <li className={styles['credit-card-benefits__nav-item']}>
+            <a href="" className={styles['credit-card-benefits__nav-link']}>
+              Rates and conditions
+            </a>
           </li>
-          <li>
-            <a href="">Cashback</a>
+          <li className={styles['credit-card-benefits__nav-item']}>
+            <a href="" className={styles['credit-card-benefits__nav-link']}>
+              Cashback
+            </a>
           </li>
-          <li>
-            <a href="">FAQ</a>
+          <li className={styles['credit-card-benefits__nav-item']}>
+            <a href="" className={styles['credit-card-benefits__nav-link']}>
+              FAQ
+            </a>
           </li>
         </ul>
       </nav>
-      <div>
-        <div>
-          <img src="" alt="" />
-          <h3></h3>
-          <p></p>
+
+      <div className={styles['credit-card-benefits__line']}></div>
+      
+      <div className={styles['credit-card-benefits__cards']}>
+        <div className={styles['credit-card-benefits__card']}>
+          <MoneyDuotoneIcon className={styles['credit-card-benefits__icon']} />
+          <h3 className={styles['credit-card-benefits__title']}>Up to 50 000 ₽</h3>
+          <p className={styles['credit-card-benefits__text']}>
+            Cash and transfers without commission and percent
+          </p>
+        </div>
+        <div className={styles['credit-card-benefits__card']}>
+          <MoneyDuotoneIcon className={styles['credit-card-benefits__icon']} />
+          <h3 className={styles['credit-card-benefits__title']}>Up to 50 000 ₽</h3>
+          <p className={styles['credit-card-benefits__text']}>
+            Cash and transfers without commission and percent
+          </p>
+        </div>
+        <div className={styles['credit-card-benefits__card']}>
+          <MoneyDuotoneIcon className={styles['credit-card-benefits__icon']} />
+          <h3 className={styles['credit-card-benefits__title']}>Up to 50 000 ₽</h3>
+          <p className={styles['credit-card-benefits__text']}>
+            Cash and transfers without commission and percent
+          </p>
+        </div>
+        <div className={styles['credit-card-benefits__card']}>
+          <MoneyDuotoneIcon className={styles['credit-card-benefits__icon']} />
+          <h3 className={styles['credit-card-benefits__title']}>Up to 50 000 ₽</h3>
+          <p className={styles['credit-card-benefits__text']}>
+            Cash and transfers without commission and percent
+          </p>
+        </div>
+        <div className={styles['credit-card-benefits__card']}>
+          <MoneyDuotoneIcon className={styles['credit-card-benefits__icon']} />
+          <h3 className={styles['credit-card-benefits__title']}>Up to 50 000 ₽</h3>
+          <p className={styles['credit-card-benefits__text']}>
+            Cash and transfers without commission and percent
+          </p>
         </div>
       </div>
+
     </div>
   )
 }
--- src/pages/LoanPage/LoanPage.tsx
+++ src/pages/LoanPage/LoanPage.tsx
@@ -1,9 +1,11 @@
+import CreditCardBenefits from '../../components/loan/CreditCardBenefits/CreditCardBenefits'
 import PlatinumCard from '../../components/loan/PlatinumCard/PlatinumCard'
 
 function LoanPage() {
   return (
     <>
       <PlatinumCard />
+      <CreditCardBenefits />
     </>
   )
 }
