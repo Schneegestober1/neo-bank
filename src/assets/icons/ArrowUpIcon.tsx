6111369b6e0a14af13a169ea1fb9f34a729097a0
Kholov Rustam <rustamkholow5@gmail.com>
2025-07-07 16:10:14 +0300

    desctop faq is ready

--- src/assets/icons/ArrowUpIcon.tsx
+++ src/assets/icons/ArrowUpIcon.tsx
@@ -10,17 +10,8 @@ const ArrowUpIcon = forwardRef<SVGSVGElement, SVGProps<SVGSVGElement>>(
         aria-hidden="true"
         focusable="false"
       >
-        <symbol
-          id="arrow-up"
-          viewBox="0 0 14 9"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M13 8L7 2L1 8"
-            stroke="currentColor"
-            strokeWidth="2"
-          />
+        <symbol id="arrow-up" viewBox="0 0 14 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M13 8L7 2L1 8" stroke="currentColor" strokeWidth="2" />
         </symbol>
       </svg>
 
