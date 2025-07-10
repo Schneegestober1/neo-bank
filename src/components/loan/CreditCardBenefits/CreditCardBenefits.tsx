be67c32ee37da3849069c4443764a9dc79c5d9e4
Kholov Rustam <rustamkholow5@gmail.com>
2025-07-10 11:55:32 +0300

    check

--- src/components/loan/CreditCardBenefits/CreditCardBenefits.tsx
+++ src/components/loan/CreditCardBenefits/CreditCardBenefits.tsx
@@ -1,63 +1,62 @@
-import { NavLink, Outlet } from 'react-router-dom'
+import { NavLink, Outlet, useLocation } from 'react-router-dom'
 import styles from '../CreditCardBenefits/credit-card-benefits.module.scss'
+import { useEffect, useRef, useState } from 'react'
 
 const CreditCardBenefits = () => {
+  const location = useLocation()
+  const navRef = useRef<HTMLUListElement>(null)
+  const [indicatorStyle, setIndicatorStyle] = useState({ left: 0, width: 0 })
+
+  useEffect(() => {
+    const updateIndicator = () => {
+      if (navRef.current) {
+        const activeLink = navRef.current.querySelector(`.${styles.active}`) as HTMLElement | null
+
+        if (activeLink) {
+          const { offsetLeft, offsetWidth } = activeLink
+          setIndicatorStyle({ left: offsetLeft, width: offsetWidth })
+        }
+      }
+    }
+
+    requestAnimationFrame(updateIndicator)
+  }, [location.pathname])
+
   return (
     <div className={styles['loan-page']}>
       <nav className={styles['loan-page__nav']}>
-        <ul className={styles['loan-page__nav-list']}>
-          <li className={styles['loan-page__nav-item']}>
-            <NavLink
-              to="about"
-              className={({ isActive }) =>
-                isActive
-                  ? `${styles['loan-page__nav-link']} ${styles[`active`]}`
-                  : styles['loan-page__nav-link']
-              }
-            >
-              About card
-            </NavLink>
-          </li>
-          <li className={styles['loan-page__nav-item']}>
-            <NavLink
-              to="rate"
-              className={({ isActive }) =>
-                isActive
-                  ? `${styles['loan-page__nav-link']} ${styles[`active`]}`
-                  : styles['loan-page__nav-link']
-              }
-            >
-              Rates and conditions
-            </NavLink>
-          </li>
-          <li className={styles['loan-page__nav-item']}>
-            <NavLink
-              to="cashback"
-              className={({ isActive }) =>
-                isActive
-                  ? `${styles['loan-page__nav-link']} ${styles[`active`]}`
-                  : styles['loan-page__nav-link']
-              }
-            >
-              Cashback
-            </NavLink>
-          </li>
-          <li className={styles['loan-page__nav-item']}>
-            <NavLink
-              to="faq"
-              className={({ isActive }) =>
-                isActive
-                  ? `${styles['loan-page__nav-link']} ${styles[`active`]}`
-                  : styles['loan-page__nav-link']
-              }
-            >
-              FAQ
-            </NavLink>
-          </li>
+        <ul className={styles['loan-page__nav-list']} ref={navRef}>
+          {[
+            { to: 'about', label: 'About card' },
+            { to: 'rate', label: 'Rates and conditions' },
+            { to: 'cashback', label: 'Cashback' },
+            { to: 'faq', label: 'FAQ' },
+          ].map(({ to, label }) => (
+            <li className={styles['loan-page__nav-item']} key={to}>
+              <NavLink
+                to={to}
+                className={({ isActive }) =>
+                  isActive
+                    ? `${styles['loan-page__nav-link']} ${styles['active']}`
+                    : styles['loan-page__nav-link']
+                }
+              >
+                {label}
+              </NavLink>
+            </li>
+          ))}
         </ul>
-      </nav>
 
-      <div className={styles['loan-page__line']}></div>
+        <div className={styles['loan-page__tab-line']}>
+          <div
+            className={styles['loan-page__tab-indicator']}
+            style={{
+              left: `${indicatorStyle.left}px`,
+              width: `${indicatorStyle.width}px`,
+            }}
+          />
+        </div>
+      </nav>
 
       <div className={styles['loan-page__content']}>
         <Outlet />
