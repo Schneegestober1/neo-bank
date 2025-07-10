62e0d9e138f4b73edbd314be36c46b64df4e6092
Kholov Rustam <rustamkholow5@gmail.com>
2025-07-10 22:39:23 +0300

    check

--- src/components/loan/CustomizeCard/CustomizeCard.tsx
+++ src/components/loan/CustomizeCard/CustomizeCard.tsx
@@ -6,30 +6,33 @@ const CustomizeCard = () => {
   return (
     <div className={styles['customize-card']}>
       <div className={styles['customize-card__container']}>
-        <div className={styles['customize-card__header']}>
-          <div className={styles['customize-card__header-top']}>
-            <h3 className={styles['customize-card__title']}>Customize your card</h3>
-            <p className={styles['customize-card__step']}>Step 1 of 5</p>
-          </div>
-
-          <div className={styles['customize-card__amount-selector']}>
-            <div className={styles['customize-card__amount-labels']}>
-              <p className={styles['customize-card__label']}>Select amount</p>
-              <p className={styles['customize-card__selected-amount']}>150 000</p>
+        <div className={styles['customize-card__top']}>
+          <div className={styles['customize-card__header']}>
+            <div className={styles['customize-card__header-top']}>
+              <h3 className={styles['customize-card__title']}>Customize your card</h3>
+              <p className={styles['customize-card__step']}>Step 1 of 5</p>
             </div>
 
-            <div className={styles['customize-card__slider']}>SLider</div>
+            <div className={styles['customize-card__amount-selector']}>
+              <div className={styles['customize-card__amount-labels']}>
+                <p className={styles['customize-card__label']}>Select amount</p>
+                <p className={styles['customize-card__selected-amount']}>150 000</p>
+              </div>
 
-            <div className={styles['customize-card__amount-range']}>
-              <p className={styles['customize-card__min-amount']}>15 000</p>
-              <p className={styles['customize-card__max-amount']}>600 000</p>
+              <div className={styles['customize-card__slider']}>SLider</div>
+
+              <div className={styles['customize-card__amount-range']}>
+                <p className={styles['customize-card__min-amount']}>15 000</p>
+                <p className={styles['customize-card__max-amount']}>600 000</p>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className={styles['customize-card__chosen-amount']}>
-          <h4 className={styles['customize-card__chosen-title']}>You have chosen the amount</h4>
-          <p className={styles['customize-card__chosen-sum']}>150 000 ₽</p>
+          <div className={styles['customize-card__chosen-amount']}>
+            <h4 className={styles['customize-card__chosen-title']}>You have chosen the amount</h4>
+            <p className={styles['customize-card__chosen-sum']}>150 000 ₽</p>
+            <div className={styles['customize-card__chosen-sum-line']}></div>
+          </div>
         </div>
 
         <form className={styles['customize-card__form']}>
